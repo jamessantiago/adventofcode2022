aaf035b9a2962fd569dfd5783ee0e471d71a9780
jamessantiago <james.r.santiago@gmail.com>
2022-11-30 20:14:11 -0600

    init

---
+++ csharp/aoc/Day_01.cs
@@ -0,0 +1,28 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc;
+
+public class Day_01 : BaseDay
+{
+    private readonly List<int> _input;
+
+    public Day_01()
+    {
+        _input = File.ReadLines(InputFilePath).Select(d => int.Parse(d)).ToList();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ csharp/aoc/Util.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc
+{
+    public static class Util
+    {
+        public static void AddOrSum<T>(this Dictionary<T, long> dict, T key, long value)
+        {
+            if (dict.ContainsKey(key)) dict[key] += value;
+            else dict.Add(key, value);
+        }
+
+        public static int XLen(this int[,] grid)
+        {
+            return grid.GetLength(0);
+        }
+
+        public static int YLen(this int[,] grid)
+        {
+            return grid.GetLength(1);
+        }
+
+        public static int XMax(this int[,] grid)
+        {
+            return grid.XLen() - 1;
+        }
+
+        public static int YMax(this int[,] grid)
+        {
+            return grid.YLen() - 1;
+        }
+
+        public static int XLen(this char[,] grid)
+        {
+            return grid.GetLength(0);
+        }
+
+        public static int YLen(this char[,] grid)
+        {
+            return grid.GetLength(1);
+        }
+
+        public static int XMax(this char[,] grid)
+        {
+            return grid.XLen() - 1;
+        }
+
+        public static int YMax(this char[,] grid)
+        {
+            return grid.YLen() - 1;
+        }
+
+        public static string HexToBinary(this char val)
+        {
+            string hexabet = "0123456789ABCDEF";
+            int lastD = hexabet.IndexOf(val);
+            return Convert.ToString(lastD, 2).PadLeft(4, '0');
+        }
+
+        public static long ToDecimal(this string val)
+        {
+            return Convert.ToInt64(val, 2);
+        }
+
+        public static void AddOrAppend(this Dictionary<int, List<(int x, int y, int z)>> dict, int key, (int x, int y, int z) value)
+        {
+            if (dict.ContainsKey(key)) dict[key].Add(value);
+            else dict.Add(key, new List<(int x, int y, int z)> { value });
+        }
+    }
+}
