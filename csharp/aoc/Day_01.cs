86f4a2fb0505ff18f53d6e7b63a020475a2ed47b
jamessantiago <james.r.santiago@gmail.com>
2022-12-01 23:26:39 -0600

    scnifes

--- csharp/aoc/Day_01.cs
+++ csharp/aoc/Day_01.cs
@@ -9,20 +9,33 @@ namespace aoc;
 
 public class Day_01 : BaseDay
 {
-    private readonly List<int> _input;
+    private readonly List<int> _input = new List<int>();
 
     public Day_01()
     {
-        _input = File.ReadLines(InputFilePath).Select(d => int.Parse(d)).ToList();
+        int i = 0;
+        _input.Add(0);
+        foreach (var line in File.ReadLines(InputFilePath))
+        {
+            if (line == "") 
+            {
+                i++;
+                _input.Add(0);
+            }
+            else
+            {
+                _input[i] += int.Parse(line);
+            }
+        }
     }
 
     public override ValueTask<string> Solve_1()
     {
-        throw new NotImplementedException();
+        return new (_input.Max().ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        return new (_input.Order().TakeLast(3).Sum().ToString());
     }
 }
---
+++ csharp/aoc/Day_02.cs
@@ -0,0 +1,63 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc;
+
+public class Day_02 : BaseDay
+{
+    private readonly List<(int, int)> _input;
+
+    public Day_02()
+    {
+        _input = File.ReadLines(InputFilePath).Select(x => ((int)x.Split()[0][0] - 64, (int)x.Split()[1][0] - 87)).ToList();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int score = 0;
+        foreach (var (p1, p2) in _input)
+        {
+            score += p2;
+            if (p1 == 1 && p2 == 3 || p1 == 2 && p2 == 1 || p1 == 3 && p2 == 2)
+            {
+                score += 0;
+            }
+            else if (p1 == p2)
+            {
+                score += 3;
+            }
+            else
+            {
+                score += 6;
+            }
+        }
+        return new (score.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int score = 0;
+        foreach (var (p1, p2) in _input)
+        {
+            if (p2 == 1)
+            {
+                score += p1 == 1 ? 3 : p1 == 2 ? 1 : 2;
+            }
+            else if (p2 == 2)
+            {
+                score += 3;
+                score += p1;
+            }
+            else
+            {
+                score += p1 == 1 ? 2 : p1 == 2 ? 3 : 1;
+                score += 6;
+            }
+        }
+        return new ValueTask<string>(score.ToString());
+    }
+}
